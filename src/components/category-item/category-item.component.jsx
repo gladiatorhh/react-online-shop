1bdff08d0e7882c478dc8cc376e13640e2d3d2d4
Arianhh <arianhaghighi732@gmail.com>
2025-03-14 01:45:36 +0330

    Add categories navigation

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,10 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 import { CategoryContainer, BackgroundImage, CategoryBody } from "./category-item.styles"
 
 const Category = ({ category }) => {
-    const { title, imageUrl } = category;
+    const { title, imageUrl,route } = category;
+
+    const navigate = useNavigate();
+
+    const navigateToRoute = () => {
+        navigate(route);
+    }
 
     return (
-        <CategoryContainer>
+        <CategoryContainer onClick={navigateToRoute}>
             <BackgroundImage background={imageUrl}></BackgroundImage>
             <CategoryBody>
                 <h2>{title}</h2>
